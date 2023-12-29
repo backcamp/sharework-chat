b4fb551b016857904761a6cbc4983225655bcc34
Jeesang Kim <jeenowden@gmail.com>
2023-12-29 12:58:41 +0900

    init commit - create project

---
+++ src/main/java/com/example/shareworkchat/ShareworkChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.shareworkchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareworkChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareworkChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shareworkchat/ShareworkChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareworkchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareworkChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
